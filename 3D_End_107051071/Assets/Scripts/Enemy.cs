bc1cc6b99a26b60cf7410bb7b6030ca8bb14ea6f
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2021-01-15 06:34:16 +0800

    14

---
+++ 3D_End_107051071/Assets/Scripts/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 30)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 30)]
+    public float stopDistance=2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 30)]
+    public float cd = 2f;
+    [Header("攻擊中心")]
+    public Transform atkpoint;
+    [Header("攻擊長度"), Range(0f, 3f)]
+    public float atklength;
+
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+    private float timer;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+
+        player = GameObject.Find("生氣").transform;
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance) 
+        {
+            timer += Time.deltaTime;
+
+            Vector3 pos = player.position;
+            pos.y = transform.position.y;
+            transform.LookAt(pos);
+
+            transform.LookAt(player);
+
+
+            transform.LookAt(player);
+            if (timer >= cd)
+            {
+                ani.SetTrigger("攻擊");
+                timer = 0;
+            }
+
+            }
+    }
+
+    private void Track()
+    {
+        nav.SetDestination(player.position);
+
+        ani.SetBool("跑步", nav.remainingDistance > stopDistance);
+        }
+}
---
+++ 3D_End_107051071/Assets/Scripts/NPC.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+
+    public bool playerInArea;
+
+    public enum NPCState
+    {
+        FirstDialog,Missioning,finish
+    }
+    public NPCState state = NPCState.FirstDialog;
+
+    /*private void Start()
+    {
+        StartCoroutine(Test());
+    }
+    private IEnumerator Test()
+    {
+        print()
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "生氣")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "生氣")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+    }
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Dialog()
+    {
+
+        dialog.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+
+        string dialogString = data.dialougA;
+
+        switch (state)
+        {
+            case NPCState.FirstDialog:
+                dialogString = data.dialougA;
+                break;
+            case NPCState.Missioning:
+                dialogString = data.dialougB;
+                break;
+            case NPCState.finish:
+                dialogString = data.dialougC;
+                break;
+
+        }
+        {
+
+        }
+       for(int i=0;i<dialogString.Length;i++)
+        {
+            //print(data.dialougA[i]);
+            textContent.text +=dialogString[i]+"";
+            yield return new WaitForSeconds(interval);
+        }
+       
+    }
+}
