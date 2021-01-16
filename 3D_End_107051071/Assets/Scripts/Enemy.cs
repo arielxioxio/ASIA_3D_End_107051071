8d30c77119efb0573ca82d53b923c880daba862d
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2021-01-17 03:33:55 +0800

    BUG炸掉

--- 3D_End_107051071/Assets/Scripts/Enemy.cs
+++ 3D_End_107051071/Assets/Scripts/Enemy.cs
@@ -13,6 +13,8 @@ public class Enemy : MonoBehaviour
     public Transform atkpoint;
     [Header("攻擊長度"), Range(0f, 3f)]
     public float atklength;
+    [Header("攻擊力"), Range(0,500)]
+    public float atk = 30;
 
 
     private Transform player;
@@ -35,27 +37,60 @@ public class Enemy : MonoBehaviour
         Attack();
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkpoint.position, atkpoint.forward * atklength);
+
+    }
+
+    private RaycastHit hit;
+    public float hp = 100;
+
+
     private void Attack()
     {
         if (nav.remainingDistance < stopDistance) 
         {
             timer += Time.deltaTime;
 
-            Vector3 pos = player.position;
+            Vector3 pos =player.position;
             pos.y = transform.position.y;
             transform.LookAt(pos);
 
-            transform.LookAt(player);
+            
 
 
-            transform.LookAt(player);
+           
             if (timer >= cd)
             {
                 ani.SetTrigger("攻擊");
                 timer = 0;
+
+                if (Physics.Raycast(atkpoint.position, atkpoint.forward,out hit, atklength, 1 << 8))
+                {
+                    hit.collider.GetComponent<Player>().Damage(atk);
+                }
             }
 
-            }
+         }
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= 30;
+        ani.SetTrigger("受傷");
+
+        if (hp <= 0) Dead();
+
+    }
+    private void Dead()
+    {
+        nav.isStopped = true;
+        enabled = false;
+        ani.SetBool ("死亡",true);
+
+ 
     }
 
     private void Track()
---
+++ 3D_End_107051071/Assets/Scripts/Player.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using Invector.vCharacterController;
+
+public class Player: MonoBehaviour
+{
+    private float hp = 100;
+    private Animator ani;
+
+    private int atkCount;
+
+    private float timer;
+    [Header("連擊間隔"), Range(0, 3)]
+    public float interval = 1;
+    [Header("攻擊中心")]
+    public Transform atkpoint;
+    [Header("攻擊長度"), Range(0f, 3f)]
+    public float atklength;
+    [Header("攻擊力"), Range(0, 500)]
+    public float atk = 30;
+
+    private void Awake()
+    {
+
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkpoint.position, atkpoint.forward * atklength);
+
+    }
+
+    private RaycastHit hit;
+
+    private void Attack()
+    {
+
+        if (atkCount < 3)
+        {
+
+
+
+
+            if (timer < interval)
+            {
+                timer += Time.deltaTime;
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    atkCount++;
+                    timer = 0;
+                    ani.SetInteger("連擊", atkCount);
+                }
+
+            }
+
+            else
+            {
+                atkCount = 0;
+                timer = 0;
+
+                if (Physics.Raycast(atkpoint.position, atkpoint.forward, out hit, atklength, 1 << 9))
+                {
+                    hit.collider.GetComponent<Enemy>().Damage(atk);
+                }
+            }
+        }
+
+        if (atkCount == 3) atkCount = 0;
+        ani.SetInteger("連擊", atkCount);
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= 30;
+        ani.SetTrigger("受傷");
+
+        if (hp <= 0) Dead();
+        
+           }
+    private void Dead()
+    {
+        ani.SetTrigger("死亡");
+
+        vThirdPersonController vt = GetComponent<vThirdPersonController>();
+        vt.lockMovement = true;
+        vt.lockRotation = true; 
+    }
+
+
+   
+
+}
